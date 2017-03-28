32af088ed8314b6b9bf72f03d46e9b4ce4024b79
Sehwan Noh <devnoh@gmail.com>
2017-03-27 23:56:50 -0700

    Initial commit

---
+++ src/main/java/devnoh/java/reactive/App.java
@@ -0,0 +1,13 @@
+package devnoh.java.reactive;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/devnoh/java/reactive/sample1/StockServer.java
@@ -0,0 +1,26 @@
+package devnoh.java.reactive.sample1;
+
+import rx.Observable;
+
+import java.util.List;
+
+public class StockServer {
+    public static Observable<StockInfo> getFeed(List<String> symbols) {
+        return Observable.create(subscriber -> {
+            while (true) {
+                symbols.stream()
+                        .map(StockInfo::fetch)
+                        .forEach(subscriber::onNext);
+
+                sleep(1000);
+            }
+        });
+    }
+
+    private static void sleep(int ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (Exception ex) {
+        }
+    }
+}
