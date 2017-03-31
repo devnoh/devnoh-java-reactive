c73600816603764cbdb58329097fdf8944f0cfb7
Sehwan Noh <devnoh@gmail.com>
2017-03-31 14:20:07 -0700

    Add Future examples

--- src/main/java/devnoh/java/reactive/ex04/FutureEx.java
+++ src/main/java/devnoh/java/reactive/ex04/FutureEx.java
@@ -1,4 +1,4 @@
-package devnoh.java.reactive.Ex04;
+package devnoh.java.reactive.ex04;
 
 import lombok.extern.slf4j.Slf4j;
 
--- src/main/java/devnoh/java/reactive/ex04/FutureEx3.java
+++ src/main/java/devnoh/java/reactive/ex04/FutureEx3.java
@@ -1,4 +1,4 @@
-package devnoh.java.reactive.Ex04;
+package devnoh.java.reactive.ex04;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -8,7 +8,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 @Slf4j
-public class FutureEx2 {
+public class FutureEx3 {
 
     public static void main(String[] args) throws InterruptedException, ExecutionException {
         ExecutorService es = Executors.newCachedThreadPool();
@@ -19,6 +19,12 @@ public class FutureEx2 {
             return "Hello";
         });
 
+        while (!future.isDone()) {
+            log.debug("> " + future.isDone());
+            Thread.sleep(500);
+        }
+
+        log.debug("> " + future.isDone());
         log.debug(future.get()); // Blocking (vs. Non-Blocking)
         log.debug("Exit");
     }
